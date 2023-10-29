198bc07863b31f038a6807860323ddb5ad1503e3
mayuresh <mayuresh.bandekar@softlabsgroup.com>
2023-10-29 23:00:12 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Dashboard from './components/pages/Dashboard';
+import CreateAdds from './components/pages/CreateAdds';
+import CreateTextMedia from './components/pages/CreateText&Media';
+import CreateText from './components/pages/CreateText';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+        <Route path='/createAdds' element={<CreateAdds />} />
+        <Route path='/createTextMedia' element={<CreateTextMedia />} />
+        <Route path='/createText' element={<CreateText />} />
+      </Routes>
+    </Router>
   );
 }
 
